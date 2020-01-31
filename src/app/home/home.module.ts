f919b708363706527a8698e31fc40f856b43cce1
Pa7ry <34232599+Pa7ry@users.noreply.github.com>
2020-01-31 14:14:07 +0100

    src

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+
+import { LoginFormComponent } from './../components/login-form/login-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [
+    HomePage,
+    LoginFormComponent
+  ]
+})
+export class HomePageModule {}
